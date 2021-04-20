75432e1d88ac1b4c16afaada7d6c0b537205e60b
mzwiener3 <82907289+mzwiener3@users.noreply.github.com>
2021-04-20 15:51:51 -0500

    Update Gemma_Data_Preparation.R

--- Gemma_Data_Preparation.R
+++ Gemma_Data_Preparation.R
@@ -1,22 +1,22 @@
-pheno <- read.csv("data/work/All_final.csv")
+pheno <- read.csv("All_final.csv")
 
-fam <- read.table("data/plink/SAP.fam")
+fam <- read.table("SAP.fam")
 fam <- data.frame(Plot=fam$V2)
 
 #GEMMA doesn't chacke phenoype order so:
 ph <- plyr::join(fam, pheno, by="Plot") # join your phenotype with Asseccion list from plink file to make sure that they are in the same order. This crucial like in rMVP. 
 
 # Save your phenotype as matrix. You can't have any column and row names, there must be pure numbers. 
-write.table(ph[,2:7], row.names = F, col.names = F, quote = F, sep = "\t", file = "gemma/phenotype4.txt")
+write.table(ph[,2:7], row.names = F, col.names = F, quote = F, sep = "\t", file = "gemma/phenotype.txt")
 
 # Phenotype names for naming output files from loop (if you want to is it, it is not necessery.)
-write.table(colnames(ph)[2:7], row.names = F, col.names = F, quote = F, sep = "\t", file = "gemma/phenotypeNames4.txt")
+write.table(colnames(ph)[2:7], row.names = F, col.names = F, quote = F, sep = "\t", file = "gemma/phenotypeNames.txt")
 
 library(bigmemory)
 
 #same story with PC. We know they are in correct order but we have to convert it to regular text file. 
-pc <- bigmemory::as.matrix(attach.big.matrix("data/rMVP/SAP.pc.desc"))
-write.table(pc, row.names = F, col.names = F, quote = F, sep = "\t", file = "gemma/covariate4.txt")
+pc <- bigmemory::as.matrix(attach.big.matrix("SAP.pc.desc"))
+write.table(pc, row.names = F, col.names = F, quote = F, sep = "\t", file = "gemma/covariate.txt")
 
 
 #install.packages("qqman") #Do it once
@@ -31,7 +31,7 @@ dat[head(order(dat$p_lrt), n=15),] # look at 10 top associated SNPs
 
 
   ##terminal
-./gemma -bfile ../data/plink/SAP -p phenotype3.txt -gk 1 -o SAPKin
+./gemma -bfile ../data/plink/SAP -p phenotype.txt -gk 1 -o SAPKin
 mv output/SAPKin.cXX.txt ./
   
   
@@ -49,13 +49,13 @@ echo ${i}
 
 
 
-a=$(sed -n ${i}p phenotypeNames4.txt)
+a=$(sed -n ${i}p phenotypeNames.txt)
 
 echo ${a}
 
 
 
-./gemma -bfile ../data/plink/SAP -p phenotype4.txt -k SAPKin.cXX.txt -c covariate4.txt  -lmm 2 -n ${i} -o ${a}
+./gemma -bfile ../data/plink/SAP -p phenotype.txt -k SAPKin.cXX.txt -c covariate.txt  -lmm 2 -n ${i} -o ${a}
 
 
 
